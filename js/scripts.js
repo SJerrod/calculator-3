c14d8fe414915d013a27310418dcf75d39c1f345
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-19 10:50:41 -0700

    add functions to scripts.js, and basic code to index.html
    co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+function subtract(number1, number2) {
+	return number1 - number2;
+}
+
+function mutiply(number1, number2) {
+	return number1 * number2;
+}
+
+function divistion(number1, number2) {
+	return number1 / number2;
+}
+
+const num1 = parseInt(prompt("What is the first number?"));
+const num2 = parseInt(prompt("What is the second number?"));
+
+
+alert(add(num1, num2));
+
