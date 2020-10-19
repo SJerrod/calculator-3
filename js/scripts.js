db055f21575d01907d90350432e091522874e6e4
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-19 11:10:53 -0700

    Add functions to convert fahrenheit and celsius
    co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,21 @@ function mutiply(number1, number2) {
 	return number1 * number2;
 }
 
-function divistion(number1, number2) {
+function divide(number1, number2) {
 	return number1 / number2;
+}
+
+function toF(celsius) {
+	return (celsius * 1.8) + 32;
+}
+
+function toC(degrees) {
+	return (degrees - 32) / 1.8;
 }
 
 const num1 = parseInt(prompt("What is the first number?"));
 const num2 = parseInt(prompt("What is the second number?"));
 
 
-alert(add(num1, num2));
+alert(toC(num1));
 
