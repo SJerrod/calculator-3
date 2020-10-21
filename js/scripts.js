ff88649525953c533f8b562c30e1443dd2bd7d89
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-21 14:47:55 -0700

    add addition code
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
+// Business Logic
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+function subtract(number1, number2) {
+	return number1 - number2;
+}
+
+function mutiply(number1, number2) {
+	return number1 * number2;
+}
+
+function divide(number1, number2) {
+	return number1 / number2;
+}
+
+// User Logic
 $(document).ready(function() {
-	function add(number1, number2) {
-		return number1 + number2;
-	}
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		event.preventDefault();
+		const num1 = parseInt($("#add1").val());
+		const num2 = parseInt($("#add2").val());
+		const result = add(num1, num2);
+			$("#output").text(result);
 
-	function subtract(number1, number2) {
-		return number1 - number2;
-	}
-
-	function mutiply(number1, number2) {
-		return number1 * number2;
-	}
-
-	function divide(number1, number2) {
-		return number1 / number2;
-	}
-
-	function toF(celsius) {
-		return (celsius * 1.8) + 32;
-	}
-
-	function toC(degrees) {
-		return (degrees - 32) / 1.8;
-	}
-
-	const num1 = parseInt(prompt("What is the first number?"));
-	const num2 = parseInt(prompt("What is the second number?"));
-
-
-	alert(toC(num1));
-
+	});
 });
+
+	// function toF(celsius) {
+	// 	return (celsius * 1.8) + 32;
+	// }
+
+	// function toC(degrees) {
+	// 	return (degrees - 32) / 1.8;
+	// }
