d164dbb42af43fea290a47d45606d0d475d57b0a
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-21 13:46:03 -0700

    touch up formatting, and add files to begin adding to calculator functions
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
-function add(number1, number2) {
-	return number1 + number2;
-}
+$(document).ready(function() {
+	function add(number1, number2) {
+		return number1 + number2;
+	}
 
-function subtract(number1, number2) {
-	return number1 - number2;
-}
+	function subtract(number1, number2) {
+		return number1 - number2;
+	}
 
-function mutiply(number1, number2) {
-	return number1 * number2;
-}
+	function mutiply(number1, number2) {
+		return number1 * number2;
+	}
 
-function divide(number1, number2) {
-	return number1 / number2;
-}
+	function divide(number1, number2) {
+		return number1 / number2;
+	}
 
-function toF(celsius) {
-	return (celsius * 1.8) + 32;
-}
+	function toF(celsius) {
+		return (celsius * 1.8) + 32;
+	}
 
-function toC(degrees) {
-	return (degrees - 32) / 1.8;
-}
+	function toC(degrees) {
+		return (degrees - 32) / 1.8;
+	}
 
-const num1 = parseInt(prompt("What is the first number?"));
-const num2 = parseInt(prompt("What is the second number?"));
+	const num1 = parseInt(prompt("What is the first number?"));
+	const num2 = parseInt(prompt("What is the second number?"));
 
 
-alert(toC(num1));
+	alert(toC(num1));
 
+});
