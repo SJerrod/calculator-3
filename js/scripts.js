3945a2d97b0445445f3347be9746c49dbb074555
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-21 15:14:28 -0700

    add subtract, multiply, and divide functions
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function subtract(number1, number2) {
 	return number1 - number2;
 }
 
-function mutiply(number1, number2) {
+function multiply(number1, number2) {
 	return number1 * number2;
 }
 
@@ -19,12 +19,31 @@ function divide(number1, number2) {
 $(document).ready(function() {
 	$("form#add").submit(function(event) {
 		event.preventDefault();
-		event.preventDefault();
 		const num1 = parseInt($("#add1").val());
 		const num2 = parseInt($("#add2").val());
 		const result = add(num1, num2);
 			$("#output").text(result);
-
+	});
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const num1 = parseInt($("#subtract1").val());
+		const num2 = parseInt($("#subtract2").val());
+		const result = subtract(num1, num2);
+			$("#output").text(result);
+	});
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		const num1 = parseInt($("#multiply1").val());
+		const num2 = parseInt($("#multiply2").val());
+		const result = multiply(num1, num2);
+			$("#output").text(result);
+	});
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		const num1 = parseInt($("#divide1").val());
+		const num2 = parseInt($("#divide2").val());
+		const result = divide(num1, num2);
+			$("#output").text(result);
 	});
 });
 
